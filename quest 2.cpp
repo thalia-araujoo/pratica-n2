25dbabbfdf4c32a9dda8651683392db26923ffa5
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2022-12-15 16:37:07 -0300

    Update quest 2.cpp

--- quest 2.cpp
+++ quest 2.cpp
@@ -1,31 +1,34 @@
-#include <stdio.h>
-// quest 2 escreva uma funÁ„o que recebe dois numeros como parametros e retorna o dobro do maior dos numeros recebidos//
+//escreva uma fun√ß√£o que recebe dois n√∫meros como par√¢metros
+// e retorna o dobro do maior dos n√∫meros recebidos.
 
-int dobro(int num1, int num2, int maior, int dobro){
-	if(num1 > num2){
-		
-		maior = num1;
-		
-	}else {
-		maior = num2;
+#include <stdio.h>
+
+int maior(int num1, int num2){
+	int d;
+	if(num1>num2){
+		d = 2 * num1;
+	} else{
+		d = 2 * num2;
 	}
 	
-	dobro = 2 * maior;
-	
-	return dobro;
+	return d;
 }
- int main(){
 
-	int n1, n2, maior, d; //maior foi criada para receber o dobro, poderia ser chamada de qualquer outro nome//
-	printf("Digite o numero: ");
-	scanf("%d",&n1);
+int main(){
 	
-	printf("Digite o numero: ");
-	scanf("%d",&n2);
+	int n1, n2, d;
 	
-	d = dobro(n1,n2); // chamando a funÁ„o//
+	printf("Digite o primeiro n√∫mero: ");
+	scanf("%d", &n1);
 	
-	printf("%d\n", n1, n2);
-	printf("O maior È: %d",d);
- 	return maior;
- }
+	printf("Digite o segundo n√∫mero: ");
+	scanf("%d", &n2);
+	
+	d = maior(n1, n2);
+	
+	printf("O dobro do maior n√∫mero √©: %d\n", d);
+	
+	return 0;
+}
+
+
